f122f1a504e787c2957b9199873eeb82d19ab5d8
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2024-03-09 08:59:49 +0530

    server host is changed

--- src/redux/saga/product.saga/product.service.js
+++ src/redux/saga/product.saga/product.service.js
@@ -1,6 +1,6 @@
 export const getProductCategoriesService = async () => {
   console.log("service");
-  let url = `http://localhost:3001/categories`;
+  let url = `http://128.199.18.34:3001/categories`;
   let options = {
     method: "GET",
   };
@@ -10,7 +10,7 @@ export const getProductCategoriesService = async () => {
 
 export const getProductDataService = async (cat_name) => {
   cat_name = cat_name.replaceAll("-", " ");
-  const url = `http://localhost:3001/products?category=${cat_name}`;
+  const url = `http://128.199.18.34:3001/products?category=${cat_name}`;
   const options = {
     method: "GET",
   };
